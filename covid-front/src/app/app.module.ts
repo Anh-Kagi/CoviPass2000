c2003a8f44e7a6f59e4cf66a070e60c0e09b61d6
Yousssssss <shadowuchiwa20@gmail.com>
2022-10-07 16:20:41 +0200

    Admin login page

--- covid-front/src/app/app.module.ts
+++ covid-front/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { ListeCentresComponent } from './liste-centres/liste-centres.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChoisirCentreComponent } from './choisir-centre/choisir-centre.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 
 @NgModule({
@@ -12,6 +14,8 @@ import { ChoisirCentreComponent } from './choisir-centre/choisir-centre.componen
     AppComponent,
     ListeCentresComponent,
     ChoisirCentreComponent,
+    AdminpageComponent,
+    AdminLoginComponent,
    
   ],
   imports: [
