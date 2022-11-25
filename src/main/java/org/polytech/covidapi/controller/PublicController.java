b2d85ad712c5c709e87d3e85dfb5d539a79e29ac
Anh-Kagi <anh.kagi@protonmail.com>
2022-11-25 15:24:06 +0100

    added rate limit on /public/
    Signed-off-by: Anh-Kagi <anh.kagi@protonmail.com>

--- src/main/java/org/polytech/covidapi/controller/PublicController.java
+++ src/main/java/org/polytech/covidapi/controller/PublicController.java
@@ -3,32 +3,44 @@ package org.polytech.covidapi.controller;
 import org.polytech.covidapi.model.Centre;
 import org.polytech.covidapi.model.Reservation;
 import org.polytech.covidapi.service.CentreService;
+import org.polytech.covidapi.service.RateLimitService;
 import org.polytech.covidapi.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/public/")
 public class PublicController {
     private final CentreService centres;
     private final ReservationService reservations;
+    private final RateLimitService rateLimit;
 
     @Autowired
-    public PublicController(CentreService centres, ReservationService reservations) {
+    public PublicController(CentreService centres, ReservationService reservations, RateLimitService rateLimit) {
         this.centres = centres;
         this.reservations = reservations;
+        this.rateLimit = rateLimit;
     }
 
     @GetMapping("/centre/")
-    public List<Centre> rechercherCentre(@RequestParam String ville) {
-        return centres.getAllByVille(ville);
-
+    public ResponseEntity<List<Centre>> rechercherCentre(@RequestParam String ville) {
+        Optional<ResponseEntity> token = rateLimit.tryConsume();
+        if (token.isPresent()) {
+            return token.get();
+        }
+        return ResponseEntity.ok(centres.getAllByVille(ville));
     }
 
     @PostMapping("/inscrire/")
-    public Reservation inscrire(@RequestParam Long centreId, String nom, String prenom, String mail, long telephone) {
-        return reservations.create(centreId, nom, prenom, mail, telephone);
+    public ResponseEntity<Reservation> inscrire(@RequestParam Long centreId, String nom, String prenom, String mail, long telephone) {
+        Optional<ResponseEntity> token = rateLimit.tryConsume();
+        if (token.isPresent()) {
+            return token.get();
+        }
+        return ResponseEntity.ok(reservations.create(centreId, nom, prenom, mail, telephone));
     }
 }
---
+++ src/main/java/org/polytech/covidapi/service/RateLimitService.java
@@ -0,0 +1,27 @@
+package org.polytech.covidapi.service;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.ConsumptionProbe;
+import io.github.bucket4j.Refill;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Optional;
+
+@Service
+public class RateLimitService {
+    private final Bucket bucket = Bucket.builder().addLimit(Bandwidth.classic(10, Refill.intervally(10, Duration.ofMinutes(1)))).build();
+
+    public Optional<ResponseEntity<Void>> tryConsume() {
+        ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
+        if (!probe.isConsumed()) {
+            return Optional.of(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .header("X-Rate-Limit-Retry-After-Seconds", String.valueOf(probe.getNanosToWaitForRefill() / 1_000_000))
+                    .build());
+        }
+        return Optional.empty();
+    }
+}
