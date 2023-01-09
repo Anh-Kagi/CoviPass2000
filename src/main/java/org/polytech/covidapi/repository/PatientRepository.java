fd822509cfafe33615665b27337af2278be63b8a
Yousssssss <shadowuchiwa20@gmail.com>
2023-01-09 13:46:58 +0100

    Routing + DAO

--- src/main/java/org/polytech/covidapi/repository/PatientRepository.java
+++ src/main/java/org/polytech/covidapi/repository/PatientRepository.java
@@ -4,7 +4,13 @@ import org.polytech.covidapi.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
+    List<Patient> trouverParNomOuPrenom(String nomPatient, String prenomPatient);
+
+
 
 }
